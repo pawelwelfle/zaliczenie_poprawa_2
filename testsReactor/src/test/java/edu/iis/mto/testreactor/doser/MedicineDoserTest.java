2e46a5fc454e530af67290903fb05a601a3fc3b5
pawelwelfle <pawel.welfle@gmail.com>
2020-09-15 18:23:09 +0200

    Test of exception

--- testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.testreactor.doser;
 
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.iis.mto.testreactor.doser.infuser.Infuser;
@@ -7,8 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Assertions;
+import static org.mockito.Mockito.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,17 +29,22 @@ class MedicineDoserTest {
     private Clock clock;
 
     private MedicineDoser medicineDoser;
-//    private Receipe receipe;
+
+
+    private DosingResult resultWithSuccess = DosingResult.SUCCESS;
 
     private static final CapacityUnit STANDARD_UNIT = CapacityUnit.MILILITER;
     private static final Capacity STANDARD_CAPACITY = Capacity.of(20, STANDARD_UNIT);
     private static final Medicine FIRST_MEDICINE = Medicine.of("First Medicine");
-    //    Medicine medicine = Medicine.of("Medicine");
+    private static final Medicine medicine = Medicine.of("First Medicine");
     private static final MedicinePackage TEST_PACKAGE = MedicinePackage.of(FIRST_MEDICINE, STANDARD_CAPACITY);
+    private static final MedicinePackage TEST_EXCEPTION_PACKAGE = MedicinePackage.of(null, STANDARD_CAPACITY);
     private static final Period STANDARD_PERIOD = Period.of(STANDARD_CAPACITY.getAmount(), TimeUnit.HOURS);
     private static final Dose TEST_DOSE = Dose.of(STANDARD_CAPACITY, STANDARD_PERIOD);
+    private static final Dose dose = Dose.of(STANDARD_CAPACITY, STANDARD_PERIOD);
 
     private static final Receipe receipe = Receipe.of(FIRST_MEDICINE, TEST_DOSE, 1);
+    private static final Receipe receipeFail = Receipe.of(null, TEST_DOSE, 1);
 
 
     @BeforeEach
@@ -45,9 +57,30 @@ class MedicineDoserTest {
     void shouldIDoseAMedicine() {
         medicineDoser.add(TEST_PACKAGE);
         medicineDoser.dose(receipe);
-
+        DosingResult result = medicineDoser.dose(receipe);
+        assertEquals(resultWithSuccess, result);
     }
 
+
+    @Test
+    void checkIfMedicinePackageIfProperCreated() {
+
+        Assertions.assertEquals(TEST_PACKAGE.getMedicine().getName(), FIRST_MEDICINE.getName(), "test medicine");
+        Assertions.assertEquals(20, STANDARD_CAPACITY.getAmount(), "test capacity");
+        Assertions.assertEquals(TEST_PACKAGE.getCapacity().getAmount(), STANDARD_CAPACITY.getAmount(), "test capacity2");
+    }
+
+
+
+    @Test
+    void shouldThrowUnavailableMedicineException() {
+        Map<Medicine, MedicinePackage> medicinesTray = new HashMap<>();
+        medicinesTray.put(receipeFail.getMedicine(), TEST_EXCEPTION_PACKAGE);
+        MedicinePackage medicinePackage = medicinesTray.get(0);
+
+        Assertions.assertThrows(UnavailableMedicineException.class,
+                () -> medicineDoser.add(TEST_EXCEPTION_PACKAGE));
+    }
 
     @Test
     void itCompiles() {
