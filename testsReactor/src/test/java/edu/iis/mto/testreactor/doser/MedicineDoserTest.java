6d30cf76b550d89a7e957980262d9a1c0d9207bb
pawelwelfle <pawel.welfle@gmail.com>
2020-09-15 17:37:38 +0200

    Set up and prepate to TESTS

--- testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -2,12 +2,52 @@ package edu.iis.mto.testreactor.doser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.TimeUnit;
 
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
+
+    @Mock
+    private Infuser infuser;
+    @Mock
+    private DosageLog dosageLog;
+    @Mock
+    private Clock clock;
+
+    private MedicineDoser medicineDoser;
+//    private Receipe receipe;
+
+    private static final CapacityUnit STANDARD_UNIT = CapacityUnit.MILILITER;
+    private static final Capacity STANDARD_CAPACITY = Capacity.of(20, STANDARD_UNIT);
+    private static final Medicine FIRST_MEDICINE = Medicine.of("First Medicine");
+    //    Medicine medicine = Medicine.of("Medicine");
+    private static final MedicinePackage TEST_PACKAGE = MedicinePackage.of(FIRST_MEDICINE, STANDARD_CAPACITY);
+    private static final Period STANDARD_PERIOD = Period.of(STANDARD_CAPACITY.getAmount(), TimeUnit.HOURS);
+    private static final Dose TEST_DOSE = Dose.of(STANDARD_CAPACITY, STANDARD_PERIOD);
+
+    private static final Receipe receipe = Receipe.of(FIRST_MEDICINE, TEST_DOSE, 1);
+
+
+    @BeforeEach
+    void setUp() {
+        medicineDoser = new MedicineDoser(infuser, dosageLog, clock);
+
+    }
+
+    @Test
+    void shouldIDoseAMedicine() {
+        medicineDoser.add(TEST_PACKAGE);
+        medicineDoser.dose(receipe);
+
+    }
+
 
     @Test
     void itCompiles() {
